aad0d94fc783d0fd989db266198aa54e93a76b09
addrj <addyrj20@gmail.com>
2026-01-12 12:51:37 +0530

    Clean commit: remove env files and DB backups

--- server/DB/info.js
+++ server/DB/info.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
-    HOST:"localhost",
-    USER:"root",
-    PASSWORD: "",
-    DB:"iot_solar",
-    DIALECT:"mysql",
+    HOST: process.env.DB_HOST || "127.0.0.1",
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "",
+    DB: process.env.DB_NAME || "iot_solar",
+    DIALECT: process.env.DB_DIALECT || "mysql",
 
     pool: {
         max: 5,
@@ -11,4 +13,25 @@ module.exports = {
         acquire: 30000,
         idle: 10000
     }
-}
+};
+
+
+
+// require("dotenv").config();
+
+// module.exports = {
+//     HOST: process.env.HOST || "127.0.0.1",
+//     USER: process.env.USER || "root",
+//     PASSWORD: process.env.PASSWORD || "",
+//     DB: process.env.DATABASE_NAME,
+//     DIALECT: process.env.DIALECT || "mysql",
+
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
+
+
